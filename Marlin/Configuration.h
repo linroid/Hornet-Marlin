04f6bf4c407e9a52db11fc0cb43f9fb7a68642c0
Lin Zhang <linroid@gmail.com>
2022-06-21 01:15:26 +0800

    Add TPU prehead option

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2001,6 +2001,12 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "TPU"
+#define PREHEAT_3_TEMP_HOTEND 225
+#define PREHEAT_3_TEMP_BED    70
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
